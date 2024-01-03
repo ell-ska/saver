c07e93cfe98fb5f57e75b4bccfba36e0511ac9fb
ell-ska <ella@skagerstrom.se>
2024-01-03 14:29:37 +0100

    change button to use border instead of outline

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -2,49 +2,52 @@ import { cva, VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/utils/classnames'
 
-export const buttonVariants = cva('inline-flex outline-1 transition', {
-  variants: {
-    variant: {
-      primary: [
-        'bg-primary outline-primary-dark rounded-full',
-        'hover:bg-primary-light',
-        'focus-visible:bg-primary-light focus-visible:outline',
-        'active:bg-primary',
-        'disabled:bg-slate-200 disabled:text-slate-500',
-      ],
-      secondary: [
-        'rounded-full outline outline-slate-800',
-        'hover:bg-primary-light',
-        'focus-visible:outline-primary-dark focus-visible:outline',
-        'active:bg-primary',
-        'disabled:text-slate-300 disabled:outline-slate-300 disabled:hover:bg-transparent',
-      ],
-      subtle: [
-        'bg-slate-100 rounded-full',
-        'hover:bg-slate-200',
-        'focus-visible:bg-transparent focus-visible:outline',
-        'active:bg-slate-100',
-        'disabled:bg-slate-100 disabled:text-slate-500',
-      ],
-      ghost: [
-        'rounded',
-        'hover:bg-slate-100',
-        'focus-visible:bg-transparent focus-visible:outline',
-        'active:bg-slate-50',
-        'disabled:text-slate-300 disabled:hover:bg-transparent',
-      ],
+export const buttonVariants = cva(
+  'inline-flex transition outline-none border border-transparent',
+  {
+    variants: {
+      variant: {
+        primary: [
+          'bg-primary rounded-full',
+          'hover:bg-primary-light',
+          'focus-visible:bg-primary-light focus-visible:border focus-visible:border-primary-dark',
+          'active:bg-primary',
+          'disabled:bg-slate-200 disabled:text-slate-500',
+        ],
+        secondary: [
+          'rounded-full border border-slate-800',
+          'hover:bg-primary-light',
+          'focus-visible:border-primary-dark',
+          'active:bg-primary',
+          'disabled:text-slate-300 disabled:border-slate-300 disabled:hover:bg-transparent',
+        ],
+        subtle: [
+          'bg-slate-100 rounded-full',
+          'hover:bg-slate-200',
+          'focus-visible:bg-transparent focus-visible:border-primary-dark',
+          'active:bg-slate-100',
+          'disabled:bg-slate-100 disabled:text-slate-500',
+        ],
+        ghost: [
+          'rounded',
+          'hover:bg-slate-100',
+          'focus-visible:bg-transparent focus-visible:border-primary-dark',
+          'active:bg-slate-50',
+          'disabled:text-slate-300 disabled:hover:bg-transparent',
+        ],
+      },
+      size: {
+        md: 'gap-2 px-4 py-2 text-base',
+        sm: 'gap-1 px-2 py-[2px] text-sm',
+        icon: 'p-1',
+      },
     },
-    size: {
-      md: 'gap-2 px-4 py-2 text-base',
-      sm: 'gap-1 px-2 py-[2px] text-sm',
-      icon: 'p-1',
+    defaultVariants: {
+      variant: 'primary',
+      size: 'md',
     },
   },
-  defaultVariants: {
-    variant: 'primary',
-    size: 'md',
-  },
-})
+)
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants>
