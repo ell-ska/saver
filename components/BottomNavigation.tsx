889e17b4e68e1b8dcb7c38d90d6b342f4923b484
ell-ska <ella@skagerstrom.se>
2024-01-08 11:50:14 +0100

    add links to bottom nav

--- components/BottomNavigation.tsx
+++ components/BottomNavigation.tsx
@@ -1,25 +1,39 @@
+import Link from 'next/link'
 import { CircleUserRound, Home, Inbox, Plus, Search } from 'lucide-react'
 
-import Button from '@/components/ui/Button'
+import { cn } from '@/utils/classnames'
+import Button, { buttonVariants } from '@/components/ui/Button'
 
 const BottomNavigation = () => {
   return (
     <nav className='absolute bottom-0 flex w-full items-center justify-between bg-white px-12 py-4'>
-      <Button variant='ghost' size='icon'>
+      <Link
+        href='/home'
+        className={cn(buttonVariants({ variant: 'ghost', size: 'icon' }))}
+      >
         <Home />
-      </Button>
-      <Button variant='ghost' size='icon'>
+      </Link>
+      <Link
+        href='/search'
+        className={cn(buttonVariants({ variant: 'ghost', size: 'icon' }))}
+      >
         <Search />
-      </Button>
+      </Link>
       <Button variant='ghost' size='icon'>
         <Plus />
       </Button>
-      <Button variant='ghost' size='icon'>
+      <Link
+        href='/sort-later'
+        className={cn(buttonVariants({ variant: 'ghost', size: 'icon' }))}
+      >
         <Inbox />
-      </Button>
-      <Button variant='ghost' size='icon'>
+      </Link>
+      <Link
+        href='/profile'
+        className={cn(buttonVariants({ variant: 'ghost', size: 'icon' }))}
+      >
         <CircleUserRound />
-      </Button>
+      </Link>
     </nav>
   )
 }
