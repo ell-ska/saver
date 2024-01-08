011f3c0070ee6dd656f40537b58944b541102582
ell-ska <ella@skagerstrom.se>
2024-01-08 13:24:11 +0100

    move sidebar resize logic to hook

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,73 +1,23 @@
 'use client'
 
-import { ElementRef, useRef, useState } from 'react'
 import Link from 'next/link'
 import { useSession } from 'next-auth/react'
 import { ChevronsLeft, CircleUserRound, Inbox } from 'lucide-react'
 
-import { useNavigation } from '@/hooks/useNavigation'
+import { useSidebar } from '@/hooks/useSidebar'
 import { cn } from '@/utils/classnames'
 import Button, { buttonVariants } from '@/components/ui/Button'
 
 const Sidebar = () => {
   const user = useSession().data?.user
-
-  const sidebarRef = useRef<ElementRef<'aside'>>(null)
-  const isRezising = useRef(false)
-  const [isTransitioning, setIsTransitioning] = useState(false)
-  const { isCollapsed, setIsCollapsed } = useNavigation()
-
-  const resize = {
-    handleMouseDown: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      event.preventDefault()
-      event.stopPropagation()
-
-      isRezising.current = true
-      document.addEventListener('mousemove', resize.handleMouseMove)
-      document.addEventListener('mouseup', resize.handleMouseUp)
-    },
-    handleMouseMove: (event: MouseEvent) => {
-      if (!isRezising.current) return
-
-      let newWidth = event.clientX
-      const maxWidth = 480
-      const minWidth = 200
-
-      if (newWidth < minWidth) newWidth = minWidth
-      if (newWidth > maxWidth) newWidth = maxWidth
-
-      if (sidebarRef.current) {
-        sidebarRef.current.style.width = `${newWidth}px`
-      }
-    },
-    handleMouseUp: () => {
-      isRezising.current = false
-      document.removeEventListener('mousemove', resize.handleMouseMove)
-      document.removeEventListener('mouseup', resize.handleMouseUp)
-    },
-  }
-
-  const collapse = () => {
-    if (sidebarRef.current) {
-      setIsCollapsed(true)
-      setIsTransitioning(true)
-
-      sidebarRef.current.style.width = '0'
-      setTimeout(() => setIsTransitioning(false), 150)
-    }
-  }
-
-  const resetWidth = () => {
-    if (!sidebarRef.current) return
-
-    setIsTransitioning(true)
-    setIsCollapsed(false)
-
-    const defaultWidth = 240
-    sidebarRef.current.style.width = `${defaultWidth}px`
-
-    setTimeout(() => setIsTransitioning(false), 150)
-  }
+  const {
+    sidebarRef,
+    isCollapsed,
+    isTransitioning,
+    collapse,
+    resetWidth,
+    resize,
+  } = useSidebar()
 
   return (
     <aside
---
+++ hooks/useSidebar.tsx
@@ -0,0 +1,73 @@
+import { ElementRef, createRef } from 'react'
+import { create } from 'zustand'
+
+type NavigationStore = {
+  sidebarRef: React.MutableRefObject<ElementRef<'aside'> | null>
+  isResizing: React.MutableRefObject<boolean | null>
+  isCollapsed: boolean
+  isTransitioning: boolean
+  collapse: () => void
+  resetWidth: () => void
+  resize: {
+    handleMouseDown: (
+      event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    ) => void
+    handleMouseMove: (event: MouseEvent) => void
+    handleMouseUp: () => void
+  }
+}
+
+export const useSidebar = create<NavigationStore>((set, get) => ({
+  sidebarRef: createRef(),
+  isResizing: createRef(),
+  isCollapsed: false,
+  isTransitioning: false,
+  collapse: () => {
+    const sidebar = get().sidebarRef.current
+    if (!sidebar) return
+
+    set({ isTransitioning: true, isCollapsed: true })
+    sidebar.style.width = '0'
+
+    setTimeout(() => set({ isTransitioning: false }), 150)
+  },
+  resetWidth: () => {
+    const sidebar = get().sidebarRef.current
+    if (!sidebar) return
+
+    set({ isTransitioning: true, isCollapsed: false })
+
+    const defaultWidth = 240
+    sidebar.style.width = `${defaultWidth}px`
+
+    setTimeout(() => set({ isTransitioning: false }), 150)
+  },
+  resize: {
+    handleMouseDown: (event) => {
+      event.preventDefault()
+      event.stopPropagation()
+
+      get().isResizing.current = true
+      document.addEventListener('mousemove', get().resize.handleMouseMove)
+      document.addEventListener('mouseup', get().resize.handleMouseUp)
+    },
+    handleMouseMove: (event) => {
+      const sidebar = get().sidebarRef.current
+      if (!get().isResizing.current || !sidebar) return
+
+      let newWidth = event.clientX
+      const maxWidth = 480
+      const minWidth = 200
+
+      if (newWidth < minWidth) newWidth = minWidth
+      if (newWidth > maxWidth) newWidth = maxWidth
+
+      sidebar.style.width = `${newWidth}px`
+    },
+    handleMouseUp: () => {
+      get().isResizing.current = false
+      document.removeEventListener('mousemove', get().resize.handleMouseMove)
+      document.removeEventListener('mouseup', get().resize.handleMouseUp)
+    },
+  },
+}))
