23541fd24afd48b636283f40a7a868dd018a8e09
ell-ska <ella@skagerstrom.se>
2024-01-08 15:20:18 +0100

    change padding for icon size

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -40,7 +40,7 @@ export const buttonVariants = cva(
       size: {
         md: 'gap-2 px-4 py-2 text-base',
         sm: 'gap-1 px-2 py-[2px] text-sm',
-        icon: 'p-1',
+        icon: 'p-[2px]',
       },
     },
     defaultVariants: {
