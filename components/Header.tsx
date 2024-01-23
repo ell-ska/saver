0001cbfa8e68e49e3a685abc85fd9a218da3fdc4
ell-ska <ella@skagerstrom.se>
2024-01-23 10:13:26 +0100

    add edit options to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { ChevronsRight, MoreVertical, PlusCircle, Search } from 'lucide-react'
 
@@ -16,50 +17,77 @@ const Header = () => {
     state.resetWidth,
   ])
 
+  // change to dynamic when implemented
+  const isEditing = false
+
   const path = usePathname()
   const isHomeRoute = path.includes('/home')
   const isBoardRoute = path.includes('/board')
   const isCardRoute = path.includes('/card')
 
   return (
-    <header className='sticky top-0 z-30 flex h-20 w-full items-center justify-between bg-white px-4 md:px-8'>
+    <header
+      className={cn(
+        'sticky top-0 z-30 flex h-20 w-full items-center justify-between bg-white px-4 md:px-8',
+      )}
+    >
       <div className='flex overflow-hidden'>
-        <Button
-          onClick={openSidebar}
-          variant='ghost'
-          size='icon'
-          className={cn('hidden', isCollapsed && 'inline-flex')}
-          icon={<ChevronsRight />}
-        />
-        {!isHomeRoute && <Breadcrumbs />}
+        {isHomeRoute && (
+          <Link
+            href='/home'
+            className='font-branding text-xl text-primary md:hidden'
+          >
+            saver
+          </Link>
+        )}
+        {!isHomeRoute && !isEditing && (
+          <>
+            <Button
+              onClick={openSidebar}
+              variant='ghost'
+              size='icon'
+              className={cn('hidden', isCollapsed && 'inline-flex')}
+              icon={<ChevronsRight />}
+            />
+            <Breadcrumbs />
+          </>
+        )}
       </div>
-      <div className='space-x-4'>
-        <Button
-          onClick={() => openMenu('add')}
-          variant='ghost'
-          size='icon'
-          className='hidden md:inline-flex'
-          icon={<PlusCircle />}
-        />
-        <Button
-          variant='ghost'
-          size='icon'
-          className='hidden md:inline-flex'
-          icon={<Search />}
-        />
-        <Button
-          onClick={() => {
-            if (isBoardRoute) {
-              openMenu('board')
-            } else if (isCardRoute) {
-              openMenu('card')
-            }
-          }}
-          variant='ghost'
-          size='icon'
-          icon={<MoreVertical />}
-        />
-      </div>
+      {isEditing && !isHomeRoute ? (
+        <div className='flex w-full items-center justify-between'>
+          <Button variant='ghost'>select all</Button>
+          <span className='text-sm text-slate-400'>items selected</span>
+          <Button variant='ghost'>cancel</Button>
+        </div>
+      ) : (
+        <div className='space-x-4'>
+          <Button
+            onClick={() => openMenu('add')}
+            variant='ghost'
+            size='icon'
+            className='hidden md:inline-flex'
+            icon={<PlusCircle />}
+          />
+          <Button
+            variant='ghost'
+            size='icon'
+            className='hidden md:inline-flex'
+            icon={<Search />}
+          />
+          <Button
+            onClick={() => {
+              if (isBoardRoute) {
+                openMenu('board')
+              } else if (isCardRoute) {
+                openMenu('card')
+              }
+            }}
+            variant='ghost'
+            size='icon'
+            icon={<MoreVertical />}
+          />
+        </div>
+      )}
     </header>
   )
 }
