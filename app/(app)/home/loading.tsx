56a5387d0a0a0014c6fc42abd72b33dcd073927d
ell-ska <ella@skagerstrom.se>
2024-01-24 10:38:14 +0100

    add loader to home page

---
+++ app/(app)/home/loading.tsx
@@ -0,0 +1,26 @@
+import Skeleton from '@/components/Skeleton'
+
+const Loading = () => {
+  return (
+    <div className='space-y-8'>
+      <div>
+        <Skeleton rounded='sm' className='mb-4 h-7 w-32 md:mb-6' />
+        <div className='grid grid-cols-[repeat(auto-fill,_minmax(18rem,_1fr))] gap-4 md:grid-cols-2 lg:grid-cols-3'>
+          <Skeleton className='h-48' />
+          <Skeleton className='h-48' />
+          <Skeleton className='h-48' />
+        </div>
+      </div>
+      <div>
+        <Skeleton rounded='sm' className='mb-4 h-7 w-32 md:mb-6' />
+        <div className='grid grid-cols-[repeat(auto-fill,_minmax(18rem,_1fr))] gap-4 md:grid-cols-2 lg:grid-cols-3'>
+          <Skeleton className='h-48' />
+          <Skeleton className='h-48' />
+          <Skeleton className='h-48' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Loading
