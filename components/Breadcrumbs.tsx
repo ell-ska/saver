23f3b3210fb9e6e8eccdae6eb37160f34c0a4f23
ell-ska <ella@skagerstrom.se>
2024-01-24 11:32:19 +0100

    add loader to breadcrumbs

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -5,19 +5,18 @@ import { ChevronLeft } from 'lucide-react'
 import { getTitle } from '@/actions/get-title'
 import { cn } from '@/utils/classnames'
 import Button from '@/components/ui/Button'
+import Skeleton from '@/components/Skeleton'
 
-type BreadcrumbsProps = {}
-
-const Breadcrumbs = ({}: BreadcrumbsProps) => {
+const Breadcrumbs = () => {
   const router = useRouter()
   const { boardId, cardId } = useParams<{ boardId: string; cardId: string }>()
 
-  const { data } = useSWR(`/title/${boardId || cardId}`, async () => {
-    return (await getTitle({ boardId, cardId })).data
-  })
-
-  if (!data) return null
-  const crumbs = [{ title: 'home', id: 'home', type: 'home' }, ...data]
+  const { data: crumbs, isLoading } = useSWR(
+    `/title/${boardId || cardId}`,
+    async () => {
+      return (await getTitle({ boardId, cardId })).data
+    },
+  )
 
   return (
     <div className='flex items-center gap-1 overflow-hidden'>
@@ -28,24 +27,45 @@ const Breadcrumbs = ({}: BreadcrumbsProps) => {
         icon={<ChevronLeft />}
         className='md:hidden'
       />
-      {crumbs.map(({ id, title, type }, index) => (
-        <div
-          key={id}
-          className='hidden items-center gap-1 overflow-hidden last:flex md:flex'
+      <div className='flex items-center gap-1 overflow-hidden'>
+        <Button
+          asLink
+          href='/home'
+          variant='ghost'
+          className='last hidden text-base last:inline-flex md:flex md:text-sm'
         >
-          <Button
-            asLink
-            href={type === 'home' ? '/home' : `/${type}/${id}`}
-            variant='ghost'
-            className='overflow-hidden text-base md:text-sm'
-          >
-            <span className='truncate md:max-w-xs'>{title}</span>
-          </Button>
-          {index !== crumbs.length - 1 && (
-            <span className='text-slate-300'>/</span>
-          )}
-        </div>
-      ))}
+          home
+        </Button>
+        <span className='hidden text-slate-300 md:inline'>/</span>
+        {isLoading && <Skeleton rounded='sm' className='h-4 w-20' />}
+        {isLoading && cardId && (
+          <>
+            <span className='hidden text-slate-300 md:inline'>/</span>
+            <Skeleton rounded='sm' className='h-4 w-20' />
+          </>
+        )}
+        {crumbs &&
+          crumbs.map(({ id, title, type }, index) => (
+            <div key={id} className='hidden items-center last:flex md:flex'>
+              <Button
+                asLink
+                href={`/${type}/${id}`}
+                variant='ghost'
+                className='overflow-hidden text-base md:text-sm'
+              >
+                <span className='truncate md:max-w-xs'>{title}</span>
+              </Button>
+              <span
+                className={cn(
+                  'ml-1 text-slate-300',
+                  index === crumbs.length - 1 && 'hidden',
+                )}
+              >
+                /
+              </span>
+            </div>
+          ))}
+      </div>
     </div>
   )
 }
